cf579ee75d44e9beaf6324f9cadf929747d34f3b
Mike M <xyntrix@gmail.com>
2014-10-08 14:18:12 -0700

    o path change

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js');document.body.appendChild(s);})();
+javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://xyntrix.github.io/turndownforwhatjs/tdfw.js');document.body.appendChild(s);})();
